fbfda169a289bbe7b8fcb42e523e5ffb69bdba96
SuperThunder <secretagentsora@gmail.com>
2014-08-06 17:17:00 -0400

    some work of noofsides
    since because of the silly laws of math you can have side a, b or c by
    itself and still solve it

--- trigcalc.py
+++ trigcalc.py
@@ -42,10 +42,10 @@ def angleinput(noofangles):
 	
 	if noofangles == 1:
 		angleA = int(raw_input("enter angle A's value in degrees "))
-	if noofangles == 2:
+	elif noofangles == 2:
 		angleA = int(raw_input("enter angle A's value in degrees "))
 		angleB = int(raw_input("enter angle B "))
-	if noofangles >= 3: #assume that if user enters angle value greater than 3 it's a mistype
+	elif noofangles >= 3: #assume that if user enters angle value greater than 3 it's a mistype
 		angleA = int(raw_input("enter angle A's value in degrees "))
 		angleB = int(raw_input("enter angle B "))
 		angleC = int(raw_input("enter angle C "))
@@ -58,31 +58,37 @@ def angleinput(noofangles):
 #getting side lengths
 def sideinput(noofsides):
 
-	
-	
 	sidea = 0
 	sideb = 0
 	sidec = 0
-
-	if noofsides == 1:
-		sidea = int(raw_input("enter side a"))
-
-	if noofsides == 2:
-		sidea = int(raw_input("enter side a"))
-		sideb = int(raw_input("enter side b"))
-
-	if noofsides >= 3:
-		sidea = int(raw_input("enter side a"))
-		sideb = int(raw_input("enter side b"))	
-		sidec = int(raw_input("enter side c"))
+	
+	#def invidiualinput(actualsideinput)
 		
+		
+	SidesToSolve = raw_input "Which sides do you know the values to? (enter a, b, and/or c, so all three would abc)
+	
+	def SideInputA # there's gotta be a better way to do this but this works so w/e
+		sidea = raw_input("Enter the value of Side A")
+		return sidea
+		
+	def SideInputB
+		sideb = raw_input("Enter the value of Side B")
+		return sideb
+	
+	def SideInputC
+		sidec = raw_input("Enter the value of Side C")
+		return sidec
+		
+	if a in SidesToSolve
+		sidea
+	
 	return(sidea, sideb, sidec)
 	
 #calculate all the values in a right triangle from an angle, a side, and the implied right angle
 def rightanglesolve(angleA, angleB, angleC, sidea, sideb, sidec):
 	
 	sidec = sidea/(math.sin(angleA))
-	sideb = sidea/(math.tan(angleA ))
+	sideb = sidea/(math.tan(angleA))
 	angleB = math.atan(sideb/sidea) #use tan for this so that the given a value is used, reduce round error
 	
 	print "\nAngle A is ", math.degrees(angleA), "\nAngle B is ", math.degrees(angleB), "\nAngle C is 90, or should be because it's the implied right angle you said existed", "\nSide A is ", sidea, "\nSide B is ", sideb, "\nSide C is ", sidec
