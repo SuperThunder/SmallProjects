99ff50ace5fd0aead12ad4f0b0c37629d7088d14
SuperThunder <secretagentsora@gmail.com>
2014-08-08 23:47:36 -0400

    trigfunctions class(!!) with 7 solving functions
    3 for right-angled with 2 terms + implied 90 (SOH CAH TOA), 3 for
    non-right and 1 shared for all sides known (Cos Law)
    TODO: Add functions for when two angles and one side are known

--- trigcalc.py
+++ trigcalc.py
@@ -3,9 +3,7 @@ print "crappytrigcalc\n"
 import math #used for trigonometric functions,
 
 def main():
-	
-	
-	
+
 	if raw_input("is the triangle right-angled (y/n)") == "y": #find whether triangle is right
 		isright = True
 	else:
@@ -14,10 +12,7 @@ def main():
 	noofangles = int(raw_input("how many angles do you know?"))
 	noofsides = int(raw_input("how many sides do you know?"))
 	
-	angleA, angleB, angleC = angleinput(noofangles) #get as many angles as the user knows (maxes at 3)
-
-	
-		
+	angleA, angleB, angleC = angleinput(noofangles) #get as many angles as the user knows (maxes at 3)	
 	sidea, sideb, sidec = sideinput(noofsides) #get as many sides as the user knows (maxes at 3)
 	
 	angleA = math.radians(angleA) #convert ro radians from degrees as Python's trig funcions use radians
@@ -25,18 +20,14 @@ def main():
 	angleC = math.radians(angleC)
 	
 	if  isright == True:
-		print "now solving a right angled triangled with the values"
+		print "now solving a right angled triangled with the values provided", "\n Label and input your triangle so that A is a known angle and C is the 90 degree angle"
 		rightanglesolve(angleA, angleB, angleC, sidea, sideb, sidec)
-		
 	else:
 		print "now solving a non-right angled triangle with the values given"
 		nonrightsolve(angleA, angleB, angleC, sidea, sideb, sidec)
 	
-	
 #getting angle values, assume user is using degrees
 def angleinput(noofangles):
-
-	
 
 	angleA = 0 # don't ask why this is here. it makes python happy.#
 	angleB = 0
@@ -64,10 +55,9 @@ def sideinput(noofsides):
 	sideb = 0
 	sidec = 0
 	
-	#def invidiualinput(actualsideinput)
+	#def invidiualinput(actualsideinput) #can't remember what I was trying to do here
 		
-		
-	SidesToSolve = raw_input("Which sides do you know the values to? (enter a, b, and/or c, so all three would abc)")
+	SidesToSolve = raw_input("Which sides do you know the values to? (enter a, b, and/or c, so all three would abc)") 
 	
 	#there's gotta be a better way to do this but this works so whatever
 	
@@ -92,16 +82,57 @@ def sideinput(noofsides):
 	
 	return(sidea, sideb, sidec)
 	
-#calculate all the values in a right triangle from an angle, a side, and the implied right angle
-def rightanglesolve(angleA, angleB, angleC, sidea, sideb, sidec):
+class TrigFunctions(object) #the idea with this class was to have all the base solving functions for certain sides be easily accessible, but it's ended up more like having all the cases for solving consolidated
 	
-	sidec = sidea/(math.sin(angleA))
-	sideb = sidea/(math.tan(angleA))
-	angleB = math.atan(sideb/sidea) #use tan for this so that the given a value is used, reduce round error
+	def AngASida(angleA, sidea) #WARNING: only call 2 term functions for right angled triangle solving!
+		sidec = sidea/(math.sin(angleA))
+		sideb = sidea/(math.tan(angleA))
+		angleB = math.atan(sideb/sidea) #using trig for this isn't really necesarry, you could do 
+		return(angleA, angleB, sidea, sideb, sidec) # angleC is not returned here because it's the known 90 degree angle
+	def AngASidb(angleA, sideb)
+		sidea = (math.tan(angleA))*sideb
+		sidec = sidea/(math.sin(angleA)) #equations here and for angleB are copied from AngASida, in future should rewrite to use given values
+		angleB = math.atan(sideb/sidea)
+		return(angleA, angleB, sidea, sideb, sidec)
+	def AngASidc(angleA, sidec)
+		sidea = (math.sin(angleA))*sidec
+		sideb = sidea/(math.tan(angleA)) #equations here and for angleB are copied from AngASida, in future should rewrite to use given values
+		angleB = math.atan(sideb/sidea)
+		return(angleA, angleB, sidea, sideb, sidec)
+	def Sidabc
+		angleA = math.acos((sidea^2-sideb^2-sidec^2)/(-2*sideb*sidec))
+		angleB = math.acos((sidea^2-sideb^2-sidec^2)/(-2*sidea*sidec))
+		angleC = math.acos((sidea^2-sideb^2-sidec^2)/(-2*sidea*sideb))
+		return(angleA, angleB, angleC, sidea, sideb, sidec)
+	def AngASidab
+		angleB = math.asin((math.sin(angleA)/sidea)*sideb)
+		angleC = 180 - angleA - angleB
+		sidec = (sidea/math.sin(angleA))*math.sin(angleC)
+		return(angleA, angleB, angleC, sidea, sideb, sidec)
+	def AngASidac	
+		angleC = math.asin((math.sin(AngleA)/sidea)*sidec)
+		angleB = 180 - angleA - angleC
+		sideb = (sidea/math.sin(angleA))*math.sin(angleB)
+		return(angleA, angleB, angleC, sidea, sideb, sidec)
+	def AngASidbc
+		sidea = math.sqrt((sideb^2+sidec^2)-2*sideb*sidec*math.cos(angleA))
+		angleB = math.acos((sidea^2-sideb^2-sidec^2)/(-2*sidea*sidec))
+		angleC = 180 - angleA - angleC
+		return(angleA, angleB, angleC, sidea, sideb, sidec)
 	
-	print "\nAngle A is ", math.degrees(angleA), "\nAngle B is ", math.degrees(angleB), "\nAngle C is 90, or should be because it's the implied right angle you said existed", "\nSide A is ", sidea, "\nSide B is ", sideb, "\nSide C is ", sidec
 	
-def nonrightsolve(angleA, angleB, angleC, sidea, sideb, sidec):
-	print "lol how do I solve this"
+	
+	#__OLD CODE__
+	#calculate all the values in a right triangle from an angle, a side, and the implied right angle 
+#def rightanglesolve(angleA, angleB, angleC, sidea, sideb, sidec):
+#	
+#	sidec = sidea/(math.sin(angleA))
+#	sideb = sidea/(math.tan(angleA))
+#	angleB = math.atan(sideb/sidea) #use tan for this so that the given a value is used, reduce round error
+#	
+#	print "\nAngle A is ", math.degrees(angleA), "\nAngle B is ", math.degrees(angleB), "\nAngle C is 90, or should be because it's the implied right angle you said existed", "\nSide A is ", sidea, "\nSide B is ", sideb, "\nSide C is ", sidec
+	
+#def nonrightsolve(angleA, angleB, angleC, sidea, sideb, sidec):
+#	print "lol how do I solve this"
 	
 main()
