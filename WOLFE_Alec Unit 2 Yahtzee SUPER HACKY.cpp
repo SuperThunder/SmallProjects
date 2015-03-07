729c8fd57fdf13fa978571f2d3d0f488d16ccdcf
SuperThunder <alexander.wolfe.10@googlemail.com>
2015-03-07 00:44:56 -0500

    added human rolling

--- WOLFE_Alec Unit 2 Yahtzee SUPER HACKY.cpp
+++ WOLFE_Alec Unit 2 Yahtzee SUPER HACKY.cpp
@@ -5,18 +5,46 @@
 #include <time.h>
 
 int main(){
-	int Die1, Die2, Die3, Die4, Die5, Rolls = 0;
-    char player;
+	int Die1, Die2, Die3, Die4, Die5, Rolls, Reroll = 0;
+    char Player;
+    bool Yahtzee = false;
 
     srand(time(NULL));
     
-    fprintf(stdout, "Would you like to play the game yourself (y) or have the computer play (c)");
-    fscanf(stdin, "%c", player);
+    fprintf(stdout, "Would you like to play the game yourself (y) or have the computer play (c)? ");
+    fscanf(stdin, "%c", &Player);
     
+    if(Player == 'y'){
+        //initial roll
+            Die5 = rand()%6+1;
+            Die4 = rand()%6+1;
+            Die3 = rand()%6+1;
+            Die2 = rand()%6+1;
+            Die1 = rand()%6+1;
+            Rolls = 1;
+            
+        while(Yahtzee == false){
+            fprintf(stdout, "Die1: %d Die2: %d Die3: %d Die4: %d Die5: %d\n", Die1, Die2, Die3, Die4, Die5);
+            fprintf(stdout, "Which number would you like to reroll for? (Die already having this value will not reroll) ");
+            fscanf(stdin, "%d", &Reroll);
+            if(Die1 != Reroll){Die1 = rand()%6+1;Rolls += 1;} //Roll dice again and add the roll to the total
+            if(Die2 != Reroll){Die2 = rand()%6+1;Rolls += 1;}
+            if(Die3 != Reroll){Die3 = rand()%6+1;Rolls += 1;}
+            if(Die4 != Reroll){Die4 = rand()%6+1;Rolls += 1;}
+            if(Die5 != Reroll){Die5 = rand()%6+1;Rolls += 1;}
+            
+            if(Die5 == Die4 && Die5 == Die3 && Die5 == Die2 && Die5 == Die1){Yahtzee = true;}
+        }
+        fprintf(stdout, "Yahtzee at %d rolls with the number %d", Rolls, Die5);
+    
+    
+    }else{
+        fprintf(stdout, "Entering computer control mode\n");
+        Rolls = 0;
         //Computer autoroll
-        while(Die5 != Die4 || Die5 != Die2 || Die5 != Die2 || Die5 != Die1){
+        while(Die5 != Die4 || Die5 != Die3 || Die5 != Die2 || Die5 != Die1){
             Rolls += 1;
-            //give int n of the array a rand val from 1-6: roll the dice
+            //give int n of the not-array a rand val from 1-6: roll the dice
             Die5 = rand()%6+1;
             Die4 = rand()%6+1;
             Die3 = rand()%6+1;
@@ -26,8 +54,7 @@ int main(){
             fprintf(stdout, "Die1: %d Die2: %d Die3: %d Die4: %d Die5: %d\n", Die1, Die2, Die3, Die4, Die5);
             //system("clear"); //looks a little nicer but runs much more slowly
         }
-        fprintf(stdout, "Yahtzee at %d rolls with the number %d", Rolls, Die5);
-
-
+        fprintf(stdout, "\nYahtzee at %d computer rolls with the number %d", Rolls, Die5);
+    }
     return 0;
 }
