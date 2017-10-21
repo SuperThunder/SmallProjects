8b941c6cf78e41755126b411b625ac7633c881d6
SuperThunder <alexander.wolfe.10@googlemail.com>
2017-10-20 22:00:17 -0400

    Reworking electricity solver with new knowledge

---
+++ ElectricFormulaCalc/NeoElectricityFormulas.py
@@ -0,0 +1,80 @@
+def parseInput(str_Input):
+	# assuming the input comes in as one string like I=2A V=2mV R=802.37kOhm
+	tokens = " ".split(str_Input)
+	
+	for token in tokens:
+		# refine - this assumes the input was done exactly right
+		parts = "=".split(token)
+		
+		print("Letter: %s"%parts[0])
+		print("Value: %s"%parts[1])
+		
+		# the value needs to be converted from something like 5V or 2.05GW to a float
+
+
+# is there a better way to store the variable values?
+dict_ElectricVariables = { 
+							# Possibly want to initialize to None
+							"current": 0.0,
+							"voltage": 0.0,
+							"time": 0.0,
+							"voltage": 0.0,
+							"energy": 0.0,
+							"resistance": 0.0,
+							"power": 0.0
+						}
+
+# Stores the relationships between the electric values (current, voltage, etc)
+# can this information be stored in a better way?
+dict_ElectricFormulas = {
+						
+						}
+						
+# dict_ElectricVariables is passed to this function
+def solveInput(dict_Variables):
+	pass
+
+''' some ideas on how to represent the relationships between variables
+
+# Store the unit values absolutely (convert from mV format)
+dict_ElecUnit = { "type": "voltage", "value": 5.0 }
+
+
+# Define a queue of operations that should be perfomed? 
+dict_ElecReltn = { "output": "voltage", "ops": ["I", "*", "R"]
+
+# For any relationship we have
+- the output variable
+- the two input variables
+- the operation(s) that must be done to and between both inputs
+
+
+'''
+
+	
+'''
+Electrical relationships (for a single resistor with DC):
+
+# Voltage: v
+v=I*R
+v=E/Q
+
+# Current: I
+I=V/R
+
+# Power: P
+
+
+# Charge: Q
+
+
+# Time: t
+
+
+# Resistance: R
+R=V/I
+
+# Energy: E
+
+
+'''
