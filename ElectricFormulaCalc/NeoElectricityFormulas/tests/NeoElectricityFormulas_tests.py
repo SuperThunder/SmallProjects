d645db3a42eacd382af27fac7c4744305f64f8a2
SuperThunder <alexander.wolfe.10@googlemail.com>
2017-10-22 22:58:18 -0400

    Fixed invalid test input

--- ElectricFormulaCalc/NeoElectricityFormulas/tests/NeoElectricityFormulas_tests.py
+++ ElectricFormulaCalc/NeoElectricityFormulas/tests/NeoElectricityFormulas_tests.py
@@ -1,11 +1,11 @@
 from nose.tools import *
 from NeoElectricityFormulas import NeoElectricityFormulas as Solver
 
+# Valid test cases - meant to run with no errors
 def test_input_simple():
 	test1 = "I=1A v=2V"
 	output = Solver.parseInput(test1)
 	print(output)
-
 
 def test_input_simple_scale():
 	print("")
@@ -19,9 +19,16 @@ def test_input_float():
 	test2 = "v=8.0535V R=85mO"
 	# Shortening Ohm to O makes things very ambiguous
 	# In the future we will want the option of 2/3 characters units (Ohm, sec, min, hr, etc)
-	test3 = "R=100O P=4923.234234"
+	test3 = "R=100O P=4923.234234W"
 	
 	tests = [test1, test2, test3]
 	for test in tests:
 		output = Solver.parseInput(test)
 		print(output)
+
+
+# todo: write invalid test cases:
+# subtle (missing unit, two periods, bad character, etc)
+# outrageous (complete garbled input)
+# random (generate some random characters and input them)
+# large (use very large numbers)
