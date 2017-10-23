a6a79203025ae515d26bc34b0dcf7a0f1ed4c9bf
SuperThunder <alexander.wolfe.10@googlemail.com>
2017-10-22 22:52:16 -0400

    Extra test case

--- ElectricFormulaCalc/NeoElectricityFormulas/tests/NeoElectricityFormulas_tests.py
+++ ElectricFormulaCalc/NeoElectricityFormulas/tests/NeoElectricityFormulas_tests.py
@@ -17,6 +17,9 @@ def test_input_float():
 	print("")
 	test1 = "P=4.0W Q=8C"
 	test2 = "v=8.0535V R=85mO"
+	# Shortening Ohm to O makes things very ambiguous
+	# In the future we will want the option of 2/3 characters units (Ohm, sec, min, hr, etc)
+	test3 = "R=100O P=4923.234234"
 	
 	tests = [test1, test2]
 	for test in tests:
