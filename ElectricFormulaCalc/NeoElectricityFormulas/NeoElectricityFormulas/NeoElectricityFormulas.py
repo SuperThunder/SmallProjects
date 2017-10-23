6dba4a18da81415cb4dd5f5388c6e8b43373f41b
SuperThunder <alexander.wolfe.10@googlemail.com>
2017-10-22 22:49:57 -0400

    Input validation mostly working now, developing test cases to get full coverage of capabilities

--- ElectricFormulaCalc/NeoElectricityFormulas/NeoElectricityFormulas/NeoElectricityFormulas.py
+++ ElectricFormulaCalc/NeoElectricityFormulas/NeoElectricityFormulas/NeoElectricityFormulas.py
@@ -1,33 +1,42 @@
-def parseInput(str_Input):
-	# assuming the input comes in as one string like I=2A V=2mV R=802.37kOhm
-	tokens = str_Input.split(" ")
-	print(tokens)
-	
-	for token in tokens:
-		# refine - this assumes the input was done exactly right
-		parts = token.split("=")
-		# the input has been split into the variable and its value
-		# but we still need to split the value into its number component, scale component, and unit (although the unit is never going to be different)
-		
-		print("Letter: %s"%parts[0])
-		print("Value: %s"%parts[1])
-		
-		
-		# the value needs to be converted from something like 5V or 2.05GW to a float
-		
 dict_ScientificPrefixes = {
-							"m": 0.001
-							"k": 1000.0
-							"M": 1000000.0
+							"u": 0.000001,
+							"m": 0.001,
+							"k": 1000.0,
+							"M": 1000000.0,
 							"G": 1000000000.0
 							}
 
+# Right now the only unit for time is seconds!
+# This describes the variable letters for the different units
+dict_ElectricUnits = {
+						"t": "time",
+						"I": "current",
+						"v": "voltage",
+						"V": "voltage",
+						"Q": "charge",
+						"R": "resistance",
+						"P": "power",
+						"E": "energy"
+						}
+
+# This describes the unit letters assigned to each unit
+dict_ElectricVariables = {
+						"s": "time",
+						"A": "current",
+						"V": "voltage",
+						"C": "charge",
+						"O": "resistance",
+						"W": "power",
+						"J": "energy"
+						}
+
 # is there a better way to store the variable values?
-dict_ElectricVariables = { 
+dict_ElectricVariableValues = { 
 							# Possibly want to initialize to None
 							"current": 0.0,
 							"voltage": 0.0,
 							"time": 0.0,
+							"charge": 0.0,
 							"energy": 0.0,
 							"resistance": 0.0,
 							"power": 0.0
@@ -43,6 +52,76 @@ list_ElectricFormulas = [
 						{"gives": "resistance", "wants": ["", ""], "formula":"" },
 						{"gives": "energy", "wants":["", ""], "formula":"" }
 						]
+
+
+
+def parseInput(str_Input):
+	import re
+	# assuming the input comes in as one string like I=2A V=2mV R=802.37kOhm
+	tokens = str_Input.split(" ")
+	#print(tokens)
+	
+	dict_InputValues = {}
+	for token in tokens:
+		# refine - this assumes the input was done exactly right
+		parts = token.split("=")
+		#print("Letter: %s"%parts[0])
+		#print("Value: %s"%parts[1])
+		
+		token_Variable = dict_ElectricUnits[parts[0]]
+		#print("Value is for %s"%token_Variable)
+		
+		# the input has been split into the variable and its value
+		# but we still need to split the value into its number component, scale component, and unit (although the unit is never going to be different)
+		# Regex to split into number and unit. integer number or decimal number followed by at least one letter
+		pattern_Value = re.compile( "^([0-9]+|[0-9]+\.[0-9]+)([a-zA-Z]+)$" )
+		pattern_match = pattern_Value.match( parts[1] )
+		token_Values = pattern_match.groups()
+		
+		print(token_Values)
+		# Get the number, should always be present
+		token_Number = token_Values[0]
+		# Get the letters following the number
+		if( len(token_Values) == 2 ):
+			token_Unit = token_Values[1]			
+		else:
+			token_Unit = "" # No unit provided - default to no prefix metric
+		
+		
+		# Currently, our system is only one letter for scale and only one letter for unit
+		# Units are assumed to be metric and time assumed to be seconds, so for now the second letter can be disregarded
+		if( len(token_Unit)==0):
+			# no unit given
+			pass
+		elif( len(token_Unit)==1 ):
+			# only one character in unit - could be the unit (V, A, etc) or the scale (m, k, M, etc)
+			# Accepting either is a sketchy feature - we may want to enforce unit only!
+			if(token_Unit in dict_ScientificPrefixes.keys()):
+				token_Prefix = dict_ScientificPrefixes[ token_Unit ]
+			elif(token_Unit in dict_ElectricVariables.keys()):
+				# We don't use the unit letter for anything, but it is valid
+				token_Prefix = 1
+			else:
+				print("Unknown SI prefix or unit letter!\t'%s'"%token_Unit)
+				
+		elif( len(token_Unit)==2 ):
+			# two chars - both scale and unit should be present
+			token_Prefix = dict_ScientificPrefixes[ token_Unit[0] ]
+			assert( token_Unit[1] in dict_ElectricVariables )
+		else:
+			print("Too many characters in unit!")
+		
+		# the value needs to be converted from something like 5V or 2.05GW to a float
+		# Once we have a float we can put the value in the dictionary by name
+		token_ScaledValue = float(token_Number) * token_Prefix
+		#print("Scaled value of %f (%s)"%(token_ScaledValue, token_Variable))
+		
+		dict_InputValues[ token_Variable ] = token_ScaledValue
+	
+	
+	return dict_InputValues
+	
+	
 						
 # dict_ElectricVariables is passed to this function
 def solveInput(dict_Variables):
--- ElectricFormulaCalc/NeoElectricityFormulas/tests/NeoElectricityFormulas_tests.py
+++ ElectricFormulaCalc/NeoElectricityFormulas/tests/NeoElectricityFormulas_tests.py
@@ -3,6 +3,22 @@ from NeoElectricityFormulas import NeoElectricityFormulas as Solver
 
 def test_input_simple():
 	test1 = "I=1A v=2V"
-	Solver.parseInput(test1)
+	output = Solver.parseInput(test1)
+	print(output)
 
 
+def test_input_simple_scale():
+	print("")
+	test1 = "I=1mA E=3MJ"
+	output = Solver.parseInput(test1)
+	print(output)
+
+def test_input_float():
+	print("")
+	test1 = "P=4.0W Q=8C"
+	test2 = "v=8.0535V R=85mO"
+	
+	tests = [test1, test2]
+	for test in tests:
+		output = Solver.parseInput(test1)
+		print(output)
